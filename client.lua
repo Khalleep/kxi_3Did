a5d379b1405ef32d877ccf8b7d78c748edfe1af8
Khalexiii <144989632+Khalleep@users.noreply.github.com>
2024-04-28 14:24:00 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,22 +1,26 @@
 local isIdDisplaying = false
-
-RegisterKeyMapping('toggleid', 'Show players ID', 'keyboard', 'pageup')
+local lastStatus = false
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
         if IsControlPressed(0, 10) then
-            isIdDisplaying = true
+            if not isIdDisplaying then
+                isIdDisplaying = true
+                NotifyIdStatus(true)
+            end
         else
-            isIdDisplaying = false
+            if isIdDisplaying then
+                isIdDisplaying = false
+                NotifyIdStatus(false)
+            end
         end
 
         if isIdDisplaying then
             local playerPed = PlayerPedId()
             local playerCoords = GetEntityCoords(playerPed)
 
-            
             DrawPlayerId(playerCoords.x, playerCoords.y, playerCoords.z + 1.2, GetPlayerServerId(PlayerId()))
 
             for _, player in ipairs(GetActivePlayers()) do
@@ -25,7 +29,6 @@ Citizen.CreateThread(function()
                 local distance = #(playerCoords - targetCoords)
 
                 if distance < 20.0 and playerPed ~= targetPed then
-                    
                     DrawPlayerId(targetCoords.x, targetCoords.y, targetCoords.z + 1.2, GetPlayerServerId(player))
                 end
             end
@@ -44,5 +47,24 @@ function DrawPlayerId(x, y, z, id)
         SetTextCentre(1)
         AddTextComponentString(tostring(id))
         DrawText(_x, _y)
+    end
+end
+
+function NotifyIdStatus(status)
+    if status ~= lastStatus then
+        if status then
+            lib.notify({
+                title = 'Player IDs',
+                description = 'Player IDs are now on',
+                type = 'info'
+            })
+        else
+            lib.notify({
+                title = 'Player IDs',
+                description = 'Player IDs are now off',
+                type = 'info'
+            })
+        end
+        lastStatus = status
     end
 end
